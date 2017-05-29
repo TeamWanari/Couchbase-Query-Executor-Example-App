d261e5063daacabf7264e04baf97bf1d0000176e
Sükein Alex <suxy@wanari.com>
2017-05-29 18:04:21 +0200

    initial commit

---
+++ src/main/java/com/wanari/cbexample/CbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.wanari.cbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanari/cbexample/CbExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wanari.cbexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CbExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
